4ca9d477d3d9c6ed219d64f5ced2a6bffa2622fc
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-28 11:51:21 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,28 +37,28 @@ client.on('message', message => {
     	message.channel.send('```md\nCharizard\n> PokeDex Number : 006\n> Egg Group : Monster , Dragon\n> Evolution : Charmander → Charmeleon (Lv.16) → Charizard (Lv.36)\nNo wild locations found.\n```');
   	}
 // Pokedex : 007
-    if (message.content === '?x') {
-    	message.channel.send('x');
+    if (message.content === '?squirtle') {
+    	message.channel.send('```md\nSquirtle\n> PokeDex Number : 007\n> Egg Group : Monster , Water A\n> Evolution : Squirtle → Wartortle (Lv.16) → Blastoise (Lv.36)\n#Map            Region  Type    Time    Rarity      Level   Item\nSeafoam Islands Kanto   Cave    M/D/N   Rare        25      -\n```');
   	}
 // Pokedex : 008
-    if (message.content === '?x') {
-    	message.channel.send('x');
+    if (message.content === '?wartortle') {
+    	message.channel.send('```md\nWartortle\n> PokeDex Number : 008\n> Egg Group : Monster , Water A\n> Evolution : Squirtle → Wartortle (Lv.16) → Blastoise (Lv.36)\nNo wild locations found.\n```');
   	}
 // Pokedex : 009
-    if (message.content === '?x') {
-    	message.channel.send('x');
+    if (message.content === '?blastoise') {
+    	message.channel.send('```md\nBlastoise\n> PokeDex Number : 009\n> Egg Group : Monster , Water A\n> Evolution : Squirtle → Wartortle (Lv.16) → Blastoise (Lv.36)\nNo wild locations found.\n```');
   	}
 // Pokedex : 010
-    if (message.content === '?x') {
-    	message.channel.send('x');
+    if (message.content === '?caterpie') {
+    	message.channel.send('```md\nCaterpie\n> PokeDex Number : 010\n> Egg Group : Bug\n> Evolution : Caterpie → Metapod (Lv.7) → Butterfree (Lv.10)\n#Map            Region  Type    Time    Rarity      Level   Item\nPattern Bush    Kanto   Grass   M/D/N   Uncommon    50-52   -\nRoute 2         Kanto   Grass   M/D/N   Rare        3-5     -\nRoute 24        Kanto   Grass   M/D/N   Uncommon    11-12   -\nRoute 25        Kanto   Grass   M/D/N   Uncommon    11-12   -\nVirdian Forest  Kanto   Grass   M/D/N   Common      5-7     -\n```');
   	}
 // Pokedex : 011
-    if (message.content === '?x') {
-    	message.channel.send('x');
+    if (message.content === '?metapod') {
+    	message.channel.send('```md\nMetapod\n> PokeDex Number : 011\n> Egg Group : Bug\n> Evolution : Caterpie → Metapod (Lv.7) → Butterfree (Lv.10)\n#Map            Region  Type    Time    Rarity      Level   Item\nPattern Bush    Kanto   Grass   M/D/N   Uncommon    50-52   -\nPattern Bush    Kanto   Grass   M/D/N   Horde       48-50   -\nRoute 24        Kanto   Grass   M/D/N   Uncommon    12      -\nRoute 25        Kanto   Grass   M/D/N   Uncommon    12      -\nVirdian Forest  Kanto   Grass   M/D/N   Uncommon    7       -\nRoute 12        Unova   Grass   M/D/N   Uncommon    48-49   -\n```');
   	}
 // Pokedex : 012
-    if (message.content === '?x') {
-    	message.channel.send('x');
+    if (message.content === '?butterfree') {
+    	message.channel.send('```md\nButterfree\n> PokeDex Number : 012\n> Egg Group : Bug , Flying\n> Evolution : Caterpie → Metapod (Lv.7) → Butterfree (Lv.10)\n#Map            Region  Type    Time    Rarity      Level   Item\nPattern Bush    Kanto   Grass   M/D/N   Rare        50-52   -\nRoute 24        Kanto   Grass   M       Uncommon    12      -\nRoute 25        Kanto   Grass   M/D     Uncommon    12      -\nPinwheel Forest Unova   Grass   M/D/N   Special     17-19   -\nRoute 12        Unova   D.Grass M/D/N   Uncommon    53-54   -\nRoute 12        Unova   Grass   M/D/N   Special     60-65   -\n```');
   	}
 // Pokedex : 013
     if (message.content === '?x') {
