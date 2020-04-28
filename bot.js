c40a8ce8e02ef3c45f1164f20ec3bdd44557f138
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-28 08:32:06 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready'! () => {
+client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('pang');
+    	message.reply('Good Idea!');
   	}
 });
 
