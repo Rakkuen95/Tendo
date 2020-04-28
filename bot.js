9f4548be331dc5631ddf388715921fa459c39bc9
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-28 08:55:42 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,25 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply(
-            'vippro'
-        );
-  	}
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
 });
 
 // THIS  MUST  BE  THIS  WAY
