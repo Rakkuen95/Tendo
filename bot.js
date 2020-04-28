7ad9e7188095fe64fa310b20363eb4f33c8581bf
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-28 09:18:50 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,6 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
-    if (message.content === 'aaa') {
-    	message.reply('bbb');
   	}
 });
 
