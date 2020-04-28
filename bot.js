1c41fcd21970e557bb74f9ffaab2701ec17936e1
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-28 10:31:16 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,15 @@ client.on('message', message => {
     	message.channel.send('https://toquoc.mediacdn.vn/2020/3/26/photo-1-15851570172391583915287-1585189253221-1585189253221232625971.gif');
   	}
     if (message.content === '?bulbasaur') {
-    	message.channel.send('\n```md\nBulbasaur\n> PokeDex Number : 001\n> Egg Group : Monster , Plant\n> Evolution : Bulbasaur → Ivysaur (Lv.16) → Venusaur (Lv.32)\n#Map            Region  Type    Time    Rarity      Level   Item\nRoute 5         Kanto   Grass   M/D/N   Rare        10      -\n```');
+    	message.channel.send('\n
+```md\n
+Bulbasaur\n> PokeDex Number : 001\n
+> Egg Group : Monster , Plant\n
+> Evolution : Bulbasaur → Ivysaur (Lv.16) → Venusaur (Lv.32)\n
+#Map            Region  Type    Time    Rarity      Level   Item\n
+Route 5         Kanto   Grass   M/D/N   Rare        10      -\n
+```
+                             ');
   	}
 });
 
