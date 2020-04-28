a0bcb92442ae0565b4d10f511422ad4a19d338bd
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-28 08:50:16 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,16 +7,16 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('
-```md
-Bulbasaur
-> PokeDex Number : 001
-> Egg Group : Monster , Plant
-> Evolution : Bulbasaur → Ivysaur (Lv.16) → Venusaur (Lv.32)
-#Map            Region  Type    Time    Rarity      Level   Item
-Route 5         Kanto   Grass   M/D/N   Rare        10      -
-```
-                      ');
+    	message.reply('pong');
+    if (message.content === 'ping') {
+    	message.reply('```md
+                        Bulbasaur
+                        > PokeDex Number : 001
+                        > Egg Group : Monster , Plant
+                        > Evolution : Bulbasaur → Ivysaur (Lv.16) → Venusaur (Lv.32)
+                        #Map            Region  Type    Time    Rarity      Level   Item
+                        Route 5         Kanto   Grass   M/D/N   Rare        10      -
+                        ```');
   	}
 });
 
