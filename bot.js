9bb3d54b370fbd6e278509ece3fc1b5f345c545f
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-28 09:06:44 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,14 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () => {
-  console.log("I am ready!");
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-client.on("message", message => {
-  if (message.author.bot) return;
-  // The process.env.PREFIX is your bot's prefix in this case.
-  if (message.content.indexOf(process.env.PREFIX) !== 0) return;
-
-  // This is the usual argument parsing we love to use.
-  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  // And our 2 real basic commands!
-  if(command === 'ping') {
-    message.channel.send('Pong!');
-  } else
-  if (command === 'blah') {
-    message.channel.send('Meh.');
-  }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.send('pong');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
