e27e58f45c6c6b6a5b1185edb158e9c9bf30b8d9
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-28 11:01:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const activities_list = [
-    "with the &help command.", 
-    "with the developers console",
-    "with some code", 
-    "with JavaScript"
-    ]; // creates an arraylist containing phrases you want your bot to switch through.
-
-client.on('ready', () => {
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.
-});
+client.on("ready", () =>{
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({
+        status: "online",  //You can show online, idle....
+        game: {
+            name: "Using !help",  //The message shown
+            type: "STREAMING" //PLAYING: WATCHING: LISTENING: STREAMING:
+        }
+    });
+ });
 
 client.on('message', message => {
     if (message.content === '?ditto') {
