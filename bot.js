6a29471d0f39a56cbb0b965dcc45aec6465001ab
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-28 10:35:13 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
     if (message.content === '?ditto') {
     	message.channel.send('https://toquoc.mediacdn.vn/2020/3/26/photo-1-15851570172391583915287-1585189253221-1585189253221232625971.gif');
   	}
+    if (message.content === '?waifu') {
+    	message.channel.send('https://i.pinimg.com/originals/fb/4a/82/fb4a827f677726e5cd64727fbfa33382.gif');
+  	}
     if (message.content === '?bulbasaur') {
     	message.channel.send('\n
 ```md\n
