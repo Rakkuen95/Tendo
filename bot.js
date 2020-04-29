d35d93977eff6bed1dc6006babce0d70ca39dc79
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-30 00:11:28 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,8 +18,25 @@ client.on('message', message => {
     if (message.content === '?random') {
     	message.channel.send(Math.floor(Math.random() * 100) + 1);
   	}
-    if (message.content === '?haha') {
-	    message.react('😄');
+    if (message.content === '?test') {
+	    message.react('👍').then(() => message.react('👎'));
+
+const filter = (reaction, user) => {
+	return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+};
+
+message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+	.then(collected => {
+		const reaction = collected.first();
+
+		if (reaction.emoji.name === '👍') {
+			message.reply('you reacted with a thumbs up.');
+		} else {
+			message.reply('you reacted with a thumbs down.');
+		}
+	})
+	.catch(collected => {
+		message.reply('you reacted with neither a thumbs up, nor a thumbs down.')
     }
 
 
