cd3484f6f6cc3bcd96007bcf3526c17ec1caeaaa
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-05-01 00:04:27 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,14 +15,9 @@ const client = new Client();
  * received from Discord
  */
 // Set the client user's presence
-bot.on('ready', () => {
-    bot.user.setStatus('available')
-    bot.user.setPresence({
-        game: {
-            name: 'with depression',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/monstercat"
-        }
+client.user.setActivity("with depression", {
+  type: "STREAMING",
+  url: "https://www.twitch.tv/monstercat"
 });
 
 client.on('message', message => {
