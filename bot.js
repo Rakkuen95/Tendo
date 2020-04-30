3fe658ee3c660fa3c2c3661245e5d037af32cff4
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-04-30 23:00:25 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,11 @@
+'use strict';
+
+/**
+ * An example of how you can send embeds
+ */
+
 // Extract the required classes from the discord.js module
-const { Client, MessageAttachment } = require('discord.js');
+const { Client, MessageEmbed } = require('discord.js');
 
 // Create an instance of a Discord client
 const client = new Client();
@@ -13,12 +19,20 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  // If the message is '!rip'
-  if (message.content === '!rip') {
-    // Create the attachment using MessageAttachment
-    const attachment = new MessageAttachment('C:\Users\ACER\Downloads\Marie.png');
-    // Send the attachment in the message channel with a content
-    message.channel.send(`${message.author},`, attachment);
+  // If the message is "how to embed"
+  if (message.content === '!embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+    const embed = new MessageEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xff0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
   }
 });
 
