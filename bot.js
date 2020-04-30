5f6cf831b56bf30cd7f8d83e3a411dc0aaebb3b0
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-05-01 03:21:07 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,7 +52,7 @@ client.on('message', message => {
     const embed = new MessageEmbed()
 	.setColor('#c500ff')
 	.setTitle('Bulbasaur')
-	.setDescription('```md\nBulbasaur\n> PokeDex Number : 001\n> Egg Group : Monster , Plant\n> Evolution : Bulbasaur → Ivysaur (Lv.16) → Venusaur (Lv.32)\n#Map                Region  Type    Time    Rarity      Level   Item\nRoute 5             Kanto   Grass   M/D/N   Rare        10      -\n```')
+	.setDescription('```md\nBulbasaur\n> PokeDex Number : 001\n> Egg Group : Monster , Plant\n> Evolution : Bulbasaur → Ivysaur (Lv.16) → Venusaur (Lv.32)\n#Map                Type    Time    Rarity      Level   Item\nRoute 5             Grass   M/D/N   Rare        10      -\n```')
 	.setThumbnail('https://img.pokemondb.net/sprites/black-white/normal/bulbasaur.png')
 	.setTimestamp()
 	.setFooter('MMOVietnamese', 'https://i.imgur.com/M89H3PF.png');
