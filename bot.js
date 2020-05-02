d8c08f59c206e6c9f6d91c09e29ccb7c45c86cc5
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-05-02 16:48:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,8 +35,8 @@ client.on('message', message => {
 	.setImage('https://attackofthefanboy.com/wp-content/uploads/2018/11/pokemon-lets-go-last-gym-leaders.jpg')
     message.channel.send(embed);
         }
-  if (message.content === '!rip') {
-    const attachment = new MessageAttachment('https://img5.goodfon.com/wallpaper/nbig/f/b6/re-zero-kara-hajimeru-isekai-seikatsu-zhizn-v-alternativno-9.jpg');
+  if (message.content === '?emilia') {
+    const attachment = new MessageAttachment('https://i.pinimg.com/originals/fb/4a/82/fb4a827f677726e5cd64727fbfa33382.gif');
     message.channel.send(attachment);
   }
 
