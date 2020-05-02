193ae892283d8c2e4d9e2de968ed91905134d103
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-05-02 22:46:16 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,13 @@ client.on('message', message => {
 	.setImage(message.author.displayAvatarURL())
     message.channel.send(embed);
         }
+	if(message.content == "!ping"){ // Check if message is "!ping"
+			message.channel.send("Pinging ...") // Placeholder for pinging ... 
+			.then((msg) => { // Resolve promise
+				msg.edit("Ping: " + (Date.now() - msg.createdTimestamp)) // Edits message with current timestamp minus timestamp of message
+			});
+		}
+
 
     if (message.content === '?quiz') {
 const quiz = require('./quiz.json');
