234679bf3a58bf7befa8f0d7438cc74c5c7e3145
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-05-06 14:57:21 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,18 @@ client.on('ready', () => {
     }, 10000);
 });
 
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
 client.on('message', message => {
     if (message.content.toLowerCase() === '?about') {
     	message.channel.send('```md\nTeam phát triển MMO Bot bao gồm\nGen 1st : Startear\nGen 2nd : liiih\nGen 3rd : dragoncv\nGen 4th : DXgamer\nGen 5th : SniderLane\n```');
