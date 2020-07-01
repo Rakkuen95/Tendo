30afe50f8e2dcd9a48bf407ec14fef51f04adc4b
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-07-01 19:22:04 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,8 +26,11 @@ client.on("message", async message => {
 	if (command === 'ping') {
 	message.channel.send(`Hello ${message.author.username}`);
 	}
-	if (mentions.<@703960869071093840>) {
-	message.channel.send('Alo');
+	if (command === "asl") {
+	let age = args[0]; // Remember arrays are 0-based!.
+	let sex = args[1];
+	let location = args[2];
+	message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
 	}
 });
 
