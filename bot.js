ea1f87c7db42afacdab3e1e24cca330f1147acb0
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-07-01 15:56:07 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,30 +9,36 @@ client.on('ready', () => {
     }, 10000);
 });
 
-client.on('message', message => {
-    if (message.content.toLowerCase() === '?random') {
-    	message.channel.send(Math.floor(Math.random() * 100) + 1);
-  	}
-    if (message.content.toLowerCase() === '?avatar') {
-    const embed = new MessageEmbed()
-	.setColor('#c500ff')
-	.setImage(message.author.displayAvatarURL())
-    message.channel.send(embed);
-        }
-    if (message.content.toLowerCase() === '?kien') {
-var kien = ["Kiên thật sự ngu ngốc\nBảo vệ Clar cũng không xong", "Kiên không sai\nChứ ai sai ?", "Bé Kiên chào cô chú đi con"];
-var recipe = Math.floor(Math.random() * kien.length);
-message.channel.send(kien[recipe]);
-        }
-    if (message.content.toLowerCase() === '?clar') {
-var clar = ["Ông trời tạo ra địa chấn\nVà Clar là điểm nhấn", "Chụy hỉu hông\nEm chạy ngang qua thấy Clar đẹp thui\nChụy hỉu hông", "Xin Clar hãy tôn trọng hàm răng của mình"];
-var recipe = Math.floor(Math.random() * clar.length);
-message.channel.send(clar[recipe]);
-        }
-    if (message.content.toLowerCase() === '?bliz') {
-var bliz = ["Bliz dạo này còn làm ở đó hông ta", "Bliz tôn trọng hàm răng của mình\nBlizcrank", "Bliz nằm trong top 8 tỷ người đẹp nhất thế giới"];
-var recipe = Math.floor(Math.random() * bliz.length);
-message.channel.send(bliz[recipe]);
-        }
+client.on("message", async message => {
+	const prefix = "!";
+
+	if (!message.author.bot) return;
+	if (!message.guild) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+
+	if (cmd === "ping") {
+		const msg = await message.channel.send('Pinging...');
+		msg.edit('Hello');
+
+	if (cmd === "say") {
+		if (message.deletable) message.delete();
+
+		if (args.length < 1 )
+			return message.reply("Nothing to say?").then(m -> m.delete(5000));
+		const roleColor = message.guild.me.displayHexColor ==== "#000000" ? "#ffffff" : message.guild.me.displayHexColor;
+			
+		if (args[0].toLowerCase() === "embed") {
+			const embed = new RichEmbed()
+				.setColor(roleColor)
+				.setDescription(args.slice(1).join(" "))
+				.setTimestamp()
+			messge.channel.send(embed);
+		} else {
+			message.channel.send(args.join(" ");
+		}
+	}
 });
 client.login(process.env.BOT_TOKEN);
