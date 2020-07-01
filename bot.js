4de24b533ec5103d34947dfd9cd614d01555e443
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-07-01 17:48:12 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,7 @@
-const { Client, MessageAttachment, MessageEmbed } = require('discord.js');
-const client = new Client();
-const activities_list = ["with Rakkuen","with Albedox","with Claris","with Ngoc"]; 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
-        client.user.setActivity(activities_list[index]);
-    }, 10000);
-});
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-client.on("message", async message => {
-	const prefix = "!";
-
-	if (!message.author.bot) return;
-	if (!message.guild) return;
-	if (!message.content.startsWith(prefix)) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/g);
-	const cmd = args.shift().toLowerCase();
-
-	if (cmd === "ping") {
-		const msg = await message.channel.send('Pinging...');
-		msg.edit('Hello');
-    }
+client.once('ready', () => {
+	console.log(message.content);
 });
 client.login(process.env.BOT_TOKEN);
