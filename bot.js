a5b4989afb584a6db96e4b6483ba1337b34664fe
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2020-07-01 18:20:17 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,26 +4,26 @@ const prefix = "!";
 const activities_list = ["with Rakkuen","with Albedox","with Claris","with Ngoc"]; 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`)
-    setInterval(() => {
+		setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
         client.user.setActivity(activities_list[index]);
-    }, 10000);
+		}, 10000);
 });
 
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
 	if (command === 'avatar') {
     const embed = new MessageEmbed()
-	.setColor('#c500ff')
+	.setColor('#000000')
+	.setDescription('Đây là Avatar của bạn nè')
 	.setImage(message.author.displayAvatarURL())
     message.channel.send(embed);
 	} else if (command === 'beep') {
 		message.channel.send('Boop.');
 	}
-	// other commands...
+
 });
 client.login(process.env.BOT_TOKEN);
