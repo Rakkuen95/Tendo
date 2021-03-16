89403b774fa53cfdd0f07d4ae2fb288ec8efb084
nhokquay759 <64446207+nhokquay759@users.noreply.github.com>
2021-03-16 15:34:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const { Client, MessageAttachment, MessageEmbed } = require('discord.js');
 const client = new Client();
 const config = require("./data.json");
 const prefix = "!";
-const activities_list = ["with Rakkuen","with VNBoostingStar","with PvP Ranked","with Bosses"]; 
+const activities_list = ["Lux","Idoly Pride","les plumes","TrySail"]; 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`)
 		setInterval(() => {
@@ -26,6 +26,11 @@ client.on("message", message => {
 	if (command === 'discord') {
 	const attachment = new MessageAttachment('https://thumbs.gfycat.com/AmbitiousInfantileIndochinesetiger-small.gif');
 	message.channel.send(attachment);
+	} 
+	if (command === 'ohaiyo') {
+	const messages = ["kimochi", "itai", "chotomate", "konichiwa"]
+	const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+	console.log(randomMessage)
 	} 
 });
 
