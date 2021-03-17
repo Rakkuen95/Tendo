7b8a5c28e14eba77ad704d98713503e790ad00e9
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-18 03:03:13 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,19 +31,35 @@ client.on("message", message => {
 		`Anh **${message.author.username}** rút ra đi mòa!`,
 		`Anh **${message.author.username}** bơi vào đây nè!`,
 		`Anh **${message.author.username}** nhớ đeo bao vào nha!`
-	]
-	const randomMessage = messages[Math.floor(Math.random() * messages.length)];
-	message.channel.send(randomMessage);
-	}
-	if (command === 'tendo') {
-	const attachment = new MessageAttachment('https://i.imgur.com/CdmJDTJ.png');
-	message.channel.send(attachment);
-	} 
-	if (command === 'rate') {
-	const rating = Math.floor(Math.random() * 100) + 1;
-	message.channel.send(`Tendo đánh giá **${message.author.username}** đạt ${rating}/100 điểm!`);
-	}
+]
+const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+message.channel.send(randomMessage);
+}
+if (command === 'tendo') {
+const attachment = new MessageAttachment('https://i.imgur.com/CdmJDTJ.png');
+message.channel.send(attachment);
+} 
+if (command === 'rate') {
+const rating = Math.floor(Math.random() * 100) + 1;
+message.channel.send(`Tendo đánh giá **${message.author.username}** đạt ${rating}/100 điểm!`);
+}
 
+
+if (command === `pic`) {    
+	const images = [
+	"https://i.pinimg.com/474x/48/b9/76/48b976e1bd6a96a375d60d920605cefe.jpg", 
+	"https://i.pinimg.com/474x/52/7a/a4/527aa4d89f61efe7e94854f417309ffc.jpg", 
+	"https://styles.redditmedia.com/t5_2ss60/styles/communityIcon_bx5asf07hi711.png?width=256&s=aa64b672872834f8d49bf3ae560543a9d9bd7ee6"
+	];
+	const image = Math.floor(Math.random() * images.length);
+	let random = new Discord.MessageEmbed()
+	.setTitle('Here is your random pic')
+	.setAuthor('hello')
+	.setImage(String([images[image]]))
+	message.channel.send(random);
+}
+	
+	
 });
 
 client.login(process.env.BOT_TOKEN);
