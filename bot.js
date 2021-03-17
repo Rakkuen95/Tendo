2f2008942c56bb803883ef50fd910795a2a22757
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-18 03:35:13 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,7 +45,7 @@ message.channel.send(`Tendo đánh giá **${message.author.username}** đạt ${
 }
 
 
-if (command === `pic`) {    
+if (command === 'pic') {    
 	const images = [
 	"https://i.pinimg.com/474x/48/b9/76/48b976e1bd6a96a375d60d920605cefe.jpg", 
 	"https://i.pinimg.com/474x/52/7a/a4/527aa4d89f61efe7e94854f417309ffc.jpg", 
