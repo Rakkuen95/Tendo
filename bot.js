12868c77a81c5061ce8f22fb2adb470dff18d406
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-18 16:50:19 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,9 +45,15 @@ client.on("message", message => {
 	if (command === 'chest') {
 	const rating = Math.floor(Math.random() * 10) + 1;
 		if ((rating > 0) && (rating < 5))
-		message.channel.send('A');
+	const embed = new MessageEmbed()
+	.setColor('#000000')
+	.setImage(message.author.displayAvatarURL())
+	message.channel.send(embed);
 		if ((rating >= 5) && (rating <= 10))
-		message.channel.send('B');
+	const embed = new MessageEmbed()
+	.setColor('#000000')
+	.setImage(message.author.displayAvatarURL())
+	message.channel.send(embed);
 	}
 });
 
