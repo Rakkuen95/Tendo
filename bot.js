82bd87db949789a677207a631bcfe82a22957394
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-23 18:08:02 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,18 +17,10 @@ client.on("message", message => {
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-if(msg.content.startsWith("!say")){
-
-/* This takes the sentence sent, and makes it an array. In this case, a list of words. It 'splits' the list up wherever it sees space.*/
-let sentence = msg.content.split(" ");
-    
-/* .shift(), alters the list. It removes the first thing in the list. This would be the "!say" word. If we assigned shift() to a variable. Like "let x = msg.shift()" ... "x" would be the word that was removed. This is handy for grabbing command words. If you used shift() again, it would remove the second, then the third, each time that you type it.*/
-sentence.shift();
-
-// Now join the list back together into a sentence with "join()" and set that as the new sentence.
-sentence = sentence.join(" ");
-msg.channel.send(sentence);
-
+    if (message.content.toLowerCase().startsWith('botspeak')){
+        message.channel.send(message.content.replace(botspeak', ''))
+        message.delete()
+    }
 }
 
 
