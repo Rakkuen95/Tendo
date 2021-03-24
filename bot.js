ae2d9df067439ca5ada023702efefb1190b48e9c
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-24 14:34:54 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,6 +18,7 @@ client.on("message", message => {
 	const command = args.shift().toLowerCase();
 
     	if (message.content.toLowerCase().startsWith(prefix + 'say')) {
+	message.delete()
 	message.channel.send(message.content.replace(prefix + 'say', ''));
     	}
 
