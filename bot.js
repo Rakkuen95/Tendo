03e9d7d240141c23360974f93b85dad5417639c6
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-25 14:43:57 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const { Client, MessageAttachment, MessageEmbed } = require('discord.js');
 const client = new Client();
 const config = require("./data.json");
 const prefix = "^";
-let coins = require ("./coins.json");
+const coins = require ("./coins.json");
  
 const activities_list = ["Lux","Idoly Pride","les plumes","TrySail"]; 
 client.on('ready', () => {
