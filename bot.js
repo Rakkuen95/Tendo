ac3aa26835e186292d7985f67826a8a29de92990
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-25 18:34:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,10 @@ const command = args.shift().toLowerCase();
   if (message.content.startsWith(prefix + "foo")) {
     message.channel.send("bar!");
   }
-});
+  if (command === "asl") {
+  let [age, sex, location] = args;
+  message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
+  }
 
+});
 client.login(process.env.BOT_TOKEN);
