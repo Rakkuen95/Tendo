2dbaca814fb671b19949999ae9a006e027331efc
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-25 18:47:59 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-let prefix = ".";
 
 client.on("message", (message) => {
 const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
