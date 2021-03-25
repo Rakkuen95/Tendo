3ac21f305ab169817ba15e48708a94134674a4e2
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-25 15:11:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ console.log(`${coinAmt} ; ${baseAmt}`);
 
 if(coinAmt === baseAmt){
 	coins[message.author.id] = {
-		coins: coins[message.author.id] + coinAmt
+		coins: coins[message.author.id].coins + coinAmt
 	};
 fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
 	if(err) console.log(err)
