9d16bd24572fadee659ab2b670475d8e23ea59e0
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-25 18:53:55 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,16 +4,15 @@ const config = require("./config.json");
 const prefix = ".";
 
 client.on("message", (message) => {
-const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+const args = message.content.slice(config.prefix).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
 
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "foo")) {
-    message.channel.send("bar!");
+
+  if (command === "hi") {
+    message.channel.send("hello!");
   }
+
   if (command === "asl") {
   let [age, sex, location] = args;
   message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
