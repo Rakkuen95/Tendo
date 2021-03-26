4bb3a0309d4bc0bcbb8e9374cbcbe8238d2b8c5a
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-27 03:41:45 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const config = require("./config.json");
 const prefix = "^";
 const fs = require("fs");
 
-const activities_list = ["Lux","Idoly Pride","les plumes","TrySail"]; 
+const activities_list = ["Lux","Tomozaki","Beta","Dustarr"]; 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`)
 	setInterval(() => {
