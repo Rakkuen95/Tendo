151a8ac6f4c7cd3b3924c871e3a9d25e436310da
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-03-30 21:40:44 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.author.bot === 'To claim, use:')) {
+  if (message.author.bot === 'To claim, use:') {
     message.channel.send(`<@&826451810280538123>`); 
   }
 });
