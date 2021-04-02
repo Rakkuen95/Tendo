2e8d8759e90d3fbd5d42c08147a7e84138d1e56c
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-04-03 02:23:44 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const filter = response => {
 	return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
 };
 
-const activities_list = ["+help"]; 
+const activities_list = ['+help','+ping']; 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`)
 	setInterval(() => {
