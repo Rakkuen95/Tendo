596ccf8f79e1e459bd8e15b1c715dcd945243133
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-04-03 20:18:26 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,7 +53,7 @@ client.on("message", message => {
 if(cooldowns[message.author.id]){
 if(cooldowns[message.author.id] > Date.now()) delete cooldowns[message.author.id];
 message.channel.send("ping");
-else message.channel.send("user still has " + Math.round((cooldowns[message.author.id] - Date.now)/minute) + " minutes left");
+else if message.channel.send("user still has " + Math.round((cooldowns[message.author.id] - Date.now)/minute) + " minutes left");
 }
 
 });
