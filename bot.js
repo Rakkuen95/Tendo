6d2c89c5c8c19ab2c1c423fa1f8001e42573e6ba
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-04-03 18:24:20 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,7 +49,7 @@ client.on("message", message => {
 
     if (talkedRecently.has(message.author.id)) {
             message.channel.send("Wait 1 minute before getting typing this again. - " + message.author);
-	else (command === "ping") {
+    } else if (command === "ping") {
         talkedRecently.add(message.author.id);
         setTimeout(() => {talkedRecently.delete(message.author.id);}, 60000);
 	message.channel.send('Gruu');
