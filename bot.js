6435694cc3551a0290b350c5cf839b40d3099425
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-07-08 16:34:28 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const { Client, MessageAttachment, MessageEmbed } = require('discord.js');
 const client = new Client();
 const data = require('./data.json');
 const prefix = "-";
-client.commands = new Discord.Collection();
 client.cooldowns = new Discord.Collection();
 const { cooldowns } = client;
 
