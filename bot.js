66fa2f0cabadca446c1700909a44925f9cc7eb2f
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-07-16 20:17:25 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,26 @@ client.on("message", async message => {
 	message.channel.send(Embed)
 	}
 
+	if (command === "user") {
+	const user = message.mentions.users.first();
+    	const Embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setThumbnail(message.author.avatarURL)
+    .addField(`${user.tag}`, `${user}`, true)
+    .addField("ID:", `${user.id}`, true)
+    .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
+    .addField("Status:", `${user.presence.status}`, true)
+    .addField("In Server", message.guild.name, true)
+    .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
+    .addField("Bot:", `${user.bot}`, true)
+    .addField("Joined The Server On:", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY")}`, true)
+    .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
+    .addField("Roles:", member.roles.map(roles => `${roles}`).join(', '), true)
+    .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
+	message.channel.send(Embed)
+	}
+
+
 	if (command === "hi") {
 	message.channel.send(`Chào `+message.author.username+`-sama`);
 	}
