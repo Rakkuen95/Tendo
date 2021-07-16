0f8aeddb0592cd10027399a363fdf70cef53f0f7
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-07-16 16:29:55 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const { Client, MessageAttachment, MessageEmbed } = require('discord.js');
+const Discord = require('discord.js');
 const client = new Client();
 const data = require('./data.json');
 const prefix = "-";
