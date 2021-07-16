ec4ba1e22a28f660dbba283b04464161a29c3d61
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-07-16 16:25:37 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on("message", async message => {
 
 	if (command === "avatar") {	
     	const user = message.mentions.users.first() || message.author;
-    	const avatarEmbed = new Discord.RichEmbed()
+    	const avatarEmbed = new MessageEmbed()
         .setColor(0x333333)
         .setAuthor(user.username)
         .setImage(user.avatarURL);
