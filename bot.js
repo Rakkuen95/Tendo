ae101d230c6fa1ed5992f24efd5f54acbd123aab
Rakkuen <64446207+Rakkuen1995@users.noreply.github.com>
2021-07-16 18:57:01 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,20 +20,21 @@ client.on("message", async message => {
 	const command = args.shift().toLowerCase();
 
 	if (command === "server") {
+	const ServerLogo = message.guild.iconURL();
     	const Embed = new Discord.MessageEmbed()
-  .setAuthor(message.guild.name, message.guild.iconURL)
-  .setColor(3447003)
-  .setDescription(`Owner: ${message.guild.owner.user.tag} (${message.guild.owner.id})`)
-  .addField('Member Count', `${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size} (${message.guild.members.filter(m=>m.user.bot).size} bots)`, true)
-  .addField('AFK Timeout', `${message.guild.afkTimeout / 60} minutes`, true)
-  .addField('AFK Channel', `${message.guild.afkChannelID === null ? 'No AFK Channel' : client.channels.get(message.guild.afkChannelID).name} (${message.guild.afkChannelID === null ? '' : message.guild.afkChannelID})`, true)
-  .addField('Location', message.guild.region, true)
-  .addField('Created', message.guild.createdAt.toLocaleString(), true)
-  .addBlankField(true)
-  .setTimestamp()
-  .setFooter(client.user.username, client.user.avatarURL);
-
-  message.channel.send(Embed);
+                .setColor('#b700ff')
+                .setTitle("Server Info")
+                .setImage(ServerLogo)
+                .setDescription(`About **${message.guild}**`)
+                .addField("**Date Created**", `Server Created on **${message.guild.createdAt.toLocaleString()}**`)
+                .addField("**Owner**", `The Owner of This Server is ${message.guild.owner}`)
+                .addField("**Member Count**", "This Server Has ` " + `${message.guild.memberCount}` + " ` **Members**")
+                .addField("**Emoji Count**", "This Server Has ` " + `${message.guild.emojis.cache.size}` + " ` **Emojis**")
+                .addField("**Roles Count**", "This Server Has ` " + `${message.guild.roles.cache.size}` + " ` **Roles**")
+                .addField("**Channels Count**", "This Server Has ` " + `${message.guild.channels.cache.size}` + " ` **Channels**")
+                .addField("**Server Logo**", "**Downlaod Server Logo**")
+                .setURL(ServerLogo)
+            message.channel.send(Embed)
 	}
 
 
