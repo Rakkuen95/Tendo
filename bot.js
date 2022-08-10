d8e416faf8b364bf8c25863e0eb4587b87f01c0d
Cheems <64446207+Cheems95@users.noreply.github.com>
2022-08-11 04:05:57 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,51 +17,14 @@ client.on("message", async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
-
-	if (command === "rroles") {
-	const role = message.mentions.roles.first();
-	if (!role)
-	return message
-	.reply('You need mention a role')
-	.then((m) => m.delete({ timeout: 1_000 }));
-	const emoji = args[1];
-	if (!emoji)
-	return message
-	.reply('You need use a valid emoji.')
-	.then((m) => m.delete({ timeout: 1_000 }));
-	const msg = await message.channel.messages.fetch(args[2] || message.id);
-	if (!role)
-	return message
-	.reply('Message not found!')
-	.then((m) => m.delete({ timeout: 1_000 }));
-	reactionRoleManager.addRole({message: msg,role,emoji,});
-	message.reply('Done').then((m) => m.delete({ timeout: 500 }));
-	}
-
-	if (command === "help") {
-	await message.channel.send("Bot nhà làm không có help.");
-	}
-
-	if (command === "cheems") {
-	const cheems = ["69","96"]
-	const randomcheems = cheems[Math.floor(Math.random() * cheems.length)];
-	await message.channel.send(randomcheems);
-	}
-
-	if (command === "avatar") {
-	await message.channel.send(`Avatar của `+message.author.username+` nè :`);
-    	const Embed = new Discord.MessageEmbed()
-        .setColor('#36393F')
-        .setImage(message.author.displayAvatarURL());
-    	await message.channel.send(Embed);
-	}
+	const rep = ["A","B","C","D"]
+	const cheems = rep[Math.floor(Math.random() * rep.length)];
 	
 	if (command === "=") {	
 	try {
 		const sentMessage = await message.channel.send(`Cheems sẽ cắn `+message.author.username+` trong **2 phút** tiếp theo!`);
 		await sentMessage.delete({ timeout: 5000 });
-		await setTimeout(function(){message.reply(`Đau không con đỏ chí ???`)}, 120*1000);
-		await sentMessage.delete({ timeout: 10000 });
+		await setTimeout(function(){message.reply(cheems)}, 120*1000);
 		} catch (error) {
 	}
 	}
@@ -70,8 +33,7 @@ client.on("message", async message => {
 	try {
 		const sentMessage = await message.channel.send(`Cheems sẽ cắn `+message.author.username+` trong **3 phút** tiếp theo!`);
 		await sentMessage.delete({ timeout: 5000 });
-		await setTimeout(function(){message.reply(`Đau không con đỏ chí ???`)}, 180*1000);
-		await sentMessage.delete({ timeout: 10000 });
+		await setTimeout(function(){message.reply(cheems)}, 180*1000);
 		} catch (error) {
 	}
 	}
