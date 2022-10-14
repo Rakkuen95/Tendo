3469ddb89a41e67f38176f7386384042bfa64c7f
Rakkuen95 <76600934+Rakkuen95@users.noreply.github.com>
2022-10-14 14:51:45 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
 	}, 10000);
 });
 
-
+//
 client.on("message", async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).split(/ +/);
